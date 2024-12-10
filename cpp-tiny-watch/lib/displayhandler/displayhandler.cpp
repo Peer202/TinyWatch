eccf2e4720974381e356d45e4911cead72ef3eae
Peer202 <Peer200@gmx.de>
2024-12-11 00:44:09 +0100

    Renaming Functions to reflect what they actually do

--- cpp-tiny-watch/lib/displayhandler/displayhandler.cpp
+++ cpp-tiny-watch/lib/displayhandler/displayhandler.cpp
@@ -6,13 +6,12 @@
 #define HOUR_GND_PIN PIN_PA4 
 #define MIN_GND_PIN PIN_PA3
 #define LED_PINS {{1,2},{1,1},{3,3},{3,2},{3,1},{3,0},{2,1},{2,4},{2,5},{1,7},{1,6},{1,5}}
-//#define HOUR_GND_PIN {1,4}
-//#define MIN_GND_PIN 
 
-#define Blink_duration 10
-#define Diplay_duration 4000
+#define BLINK_DURATION 1
+//#define Diplay_duration 900
+#define Diplay_duration 200
 
-void TogglePin(int Port,int PinNumber)
+void TogglePinOutLevel(int Port,int PinNumber)
 { 
   int Pinmap[] = {PIN0_bm,PIN1_bm,PIN2_bm,PIN3_bm,PIN4_bm,PIN5_bm,PIN6_bm,PIN7_bm};
 
@@ -32,7 +31,7 @@ void TogglePin(int Port,int PinNumber)
   }
 }
 
-void TogglePinDir(int Port,int PinNumber)
+void SetPinDirOut(int Port,int PinNumber)
 { 
   int Pinmap[] = {PIN0_bm,PIN1_bm,PIN2_bm,PIN3_bm,PIN4_bm,PIN5_bm,PIN6_bm,PIN7_bm};
   switch (Port)
@@ -57,11 +56,11 @@ int initdisplay()
     int pinsLed [][2] = LED_PINS;
     for (int i = 0; i < 12; i++)
     {
-      TogglePinDir(pinsLed[i][0],pinsLed[i][1]);
+      SetPinDirOut(pinsLed[i][0],pinsLed[i][1]);
     }
 
-    TogglePinDir(1,4); // Hour Pin
-    TogglePinDir(1,3); // Minute Pin
+    SetPinDirOut(1,4); // Hour Pin
+    SetPinDirOut(1,3); // Minute Pin
 }
 
 
